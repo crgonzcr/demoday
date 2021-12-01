ab6361e61537cd718f66092bad0333f3c81dfd58
Cristofer Gonzalez <dark_rain-zero@hotmail.com>
2021-12-01 17:21:20 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras import preprocessing
+import time
+
+fig = plt.figure()
+
+st.title("Visual Anomaly Detection for Sewerage")
+st.header("Visual Anomaly Detection for Sewerage 2")
+st.markdown("Visual Anomaly Detection for Sewerage 💦")
+
+st.text("Upload a Sewerage Image for image classification as health or anomaly")
+
+###model = tf.keras.models.load_model("saved_model/mdl_wts.hdf5")
+
+def main():
+    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
+    class_btn = st.button("Classify")
+    if file_uploaded is not None:    
+        image = Image.open(file_uploaded)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+    if class_btn:
+        if file_uploaded is None:
+            st.write("Invalid command, please upload an image")
+        else:
+            with st.spinner('Model working....'):
+                plt.imshow(image)
+                plt.axis("off")
+                predictions = predict(image)
+                time.sleep(1)
+                st.success('Classified')
+                st.write(predictions)
+                st.pyplot(fig)
+
+
+def predict(image):
+    classifier_model = "VADS_model.h5"
+    IMAGE_SHAPE = (224, 224,3)
+    model = load_model(classifier_model, compile=False, custom_objects={'KerasLayer': hub.KerasLayer})
+    test_image = image.resize((224,224))
+    test_image = preprocessing.image.img_to_array(test_image)
+    test_image = test_image / 255.0
+    test_image = np.expand_dims(test_image, axis=0)
+    class_names = [
+          'Healthy', 
+          'Anomalous']
+    predictions = model.predict(test_image)
+    scores = tf.nn.softmax(predictions[0])
+    scores = scores.numpy()
+    results = {
+          'Healthy': 0,
+          'Anomalous': 0
+}
+
+    
+    result = f"{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence." 
+    return result
+    
+
+if __name__ == "__main__":
+    main()
