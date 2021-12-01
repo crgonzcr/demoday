9bc991e6d88ee4dde3f46beb0f53b63b21f458af
Cristofer Gonzalez <dark_rain-zero@hotmail.com>
2021-12-01 17:26:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PIL import Image
-import matplotlib.pyplot as plt
-import tensorflow_hub as hub
-import tensorflow as tf
-import numpy as np
-from tensorflow import keras
-from tensorflow.keras.models import load_model
+#import matplotlib.pyplot as plt
+#import tensorflow_hub as hub
+#import tensorflow as tf
+#import numpy as np
+#from tensorflow import keras
+#from tensorflow.keras.models import load_model
 from tensorflow.keras import preprocessing
 import time
 
