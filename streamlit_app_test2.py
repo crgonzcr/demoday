400d89cb069e38e22b0bfce472e3ae7fc64d7d2a
Cristofer Gonzalez <dark_rain-zero@hotmail.com>
2021-12-01 22:52:10 -0300

    Update and rename streamlit_app.py to streamlit_app_test2.py

--- streamlit_app_test2.py
+++ streamlit_app_test2.py
@@ -18,7 +18,7 @@ if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-    resized = cv2.resize(opencv_image,(224,224))
+    resized = cv2.resize(opencv_image,(160,160))
     # Now do something with the image! For example, let's display it:
     st.image(opencv_image, channels="RGB")
 
