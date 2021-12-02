b14ffe16865419ee95b6214ec9f3ab9bdd562af6
Cristofer Gonzalez <dark_rain-zero@hotmail.com>
2021-12-02 17:47:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,15 @@ model = tf.keras.models.load_model("saved_model/mdl_wts.hdf5")
 uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
 map_dict = {0: 'dog',
-            1: 'horse'}
+            1: 'horse',
+            2: 'elephant',
+            3: 'butterfly',
+            4: 'chicken',
+            5: 'cat',
+            6: 'cow',
+            7: 'sheep',
+            8: 'spider',
+            9: 'squirrel'}
 
 
 if uploaded_file is not None:
@@ -18,7 +26,7 @@ if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-    resized = cv2.resize(opencv_image,(160,160))
+    resized = cv2.resize(opencv_image,(224,224))
     # Now do something with the image! For example, let's display it:
     st.image(opencv_image, channels="RGB")
 
