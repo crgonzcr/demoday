6a3ae8a87c77189aac499e01433226ab9aef4858
Cristofer Gonzalez <dark_rain-zero@hotmail.com>
2021-12-03 19:12:25 -0300

    Update streamlit_app_video.py

--- streamlit_app_video.py
+++ streamlit_app_video.py
@@ -8,34 +8,15 @@ from tensorflow.keras import preprocessing
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
 model = tf.keras.models.load_model("saved_model/model.hdf5")
-### load file
-uploaded_file = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
 
 map_dict = {0: 'Healthy',
           1: 'Anomalous'}
 
+st.title("Visual Anomaly Detection for Sewerage")
+st.header("Visual Anomaly Detection for Sewerage 2")
+st.markdown("Visual Anomaly Detection for Sewerage 💦")
 
-if uploaded_file is not None:
-   image = Image.open(uploaded_file)
-
-   st.image(image, caption='Uploaded Image', use_column_width=True)    
-
-   test_image = image.resize((160,160))
-   test_image = preprocessing.image.img_to_array(test_image)
-   test_image = np.expand_dims(test_image, axis=0)
-   class_names = [
-           'Healthy', 
-           'Anomalous']
-   Genrate_pred = st.button("Generate Prediction")    
-   if Genrate_pred:
-       predictions = model.predict(test_image)
-       scores = tf.nn.softmax(predictions[0])
-       if (0 < predictions < 5):
-          st.title("Predicted Label for the image is Healthy")
-       else:
-          st.title("Predicted Label for the image is Anomalous")
-        
-
+st.text("Upload a Sewerage Image for image classification as health or anomaly")
 
 uploaded_video = st.file_uploader("Choose video", type=["mp4", "avi"])
 frame_skip = 100 # display every 300 frames
@@ -54,6 +35,9 @@ if uploaded_video is not None: # run only when user uploads video
     vidcap = cv2.VideoCapture(vid) # load video from disk
     cur_frame = 0
     success = True
+
+    st.text("Frames")
+    frame = st.slider("Selecciona el N° de frames:", 100, 150, 300)
 
     while success:
         success, frame = vidcap.read() # get next frame from video
