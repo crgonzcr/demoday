4e0fdf761e0b72da6dbfb2e828a471f1df1b4575
Cristofer Gonzalez <dark_rain-zero@hotmail.com>
2021-12-02 21:43:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ map_dict = {'Healthy': 0,
 
 if uploaded_file is not None:
     # Convert the file to an opencv image.
+    st.image(Image.open(uploaded_file))
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
