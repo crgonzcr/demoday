557110fbb6a6bab5d08690c1d767aff8aeb8d4b9
Cristofer Gonzalez <dark_rain-zero@hotmail.com>
2021-12-02 21:59:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,21 +17,30 @@ map_dict = {'Healthy': 0,
 if uploaded_file is not None:
     # Convert the file to an opencv image.
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
-    file_bytes = np.asarray(image, dtype='uint8')
+    
+    test_image = image.resize((160,160))
+    test_image = preprocessing.image.img_to_array(test_image)
+    test_image = test_image / 127.5
+    test_image = np.expand_dims(test_image, axis=0)
+    class_names = [
+          'Healthy', 
+          'Anomalous']
+          
+#     st.image(image, caption='Uploaded Image', use_column_width=True)
+#     file_bytes = np.asarray(image, dtype='uint8')
           
 #     file_bytes = np.asarray(bytearray(image), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-    resized = cv2.resize(opencv_image,(160,160))
-    #Now do something with the image! For example, let's display it:
-    st.image(opencv_image, channels="RGB")
+#     opencv_image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+#     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+#     resized = cv2.resize(opencv_image,(160,160))
+#     #Now do something with the image! For example, let's display it:
+#     st.image(opencv_image, channels="RGB")
 
-    resized = mobilenet_v2_preprocess_input(resized)
-    img_reshape = resized[np.newaxis,...]
+#     resized = mobilenet_v2_preprocess_input(resized)
+#     img_reshape = resized[np.newaxis,...]
 
     Genrate_pred = st.button("Generate Prediction")    
     if Genrate_pred:
-       prediction = model.predict(img_reshape).argmax()
+       prediction = model.predict(test_image).argmax()
        st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
 
