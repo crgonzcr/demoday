978d0a55561c1e7c47d4c79dfca5fb93f319c8cc
Cristofer Gonzalez <dark_rain-zero@hotmail.com>
2021-12-02 22:31:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if uploaded_file is not None:
 #    pred = model.predict(img_array)
 
 #     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)    
+   st.image(image, caption='Uploaded Image', use_column_width=True)    
 
    test_image = image.resize((160,160))
    test_image = preprocessing.image.img_to_array(test_image)
